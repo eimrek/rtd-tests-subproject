786dc70b63ff18407d2c736f36d11f1b43c824c4
Kristjan Eimre <kristjaneimre@gmail.com>
2024-10-04 13:55:26 +0300

    update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,5 +1,11 @@
 # Configuration file for the Sphinx documentation builder.
 
+import os
+import sys
+
+# Add your project's root directory to the sys.path to find your modules
+sys.path.insert(0, os.path.abspath('../../'))
+
 # -- Project information
 
 project = 'rtd_tests_sub'
